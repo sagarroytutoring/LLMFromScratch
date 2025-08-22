3c384912ae50aa50572ada5561501a1b1437605b
Sagar Roy <sagarroytutoring@gmail.com>
2025-08-22 15:59:27 -0700

    Wrote some tests and ideas

--- autodiff/__init__.py
+++ autodiff/__init__.py
@@ -33,23 +33,122 @@
 # - higher order derivatives maybe?
 
 
-class Expression:
-    def __add__(self, other):
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class VariableAssignmentError(BaseException): pass
+
+
+class Expression(ABC):
+    def __init__(self, subexps: tuple['Expression', ...]):
+        self._subexps = subexps
+        for exp in subexps:
+            if isinstance(exp, Variable):
+                exp._add_dependent_expression(self)
+        self._val = None
+
+    def __add__(self, other: 'Expression') -> 'AdditionExpression':
+        return AdditionExpression(self, other)
+
+    def __mul__(self, other: 'Expression') -> 'MultiplicationExpression':
+        return MultiplicationExpression(self, other)
+
+    @property
+    def val(self):
+        if self._val is None:
+            self._val = self._calc()
+        return self._val
+
+    @val.setter
+    def val(self, v):
+        self.set(v)
+
+    def set(self, v):
+        raise NotImplementedError('This expression does not allow value assignment')
+
+    def unset(self):
+        self._val = None
+
+    @abstractmethod
+    def _calc(self):
         pass
 
-    def __mul__(self, other):
-        pass
+
+class AdditionExpression(Expression):
+
+    def __init__(self, left_term: Expression, right_term: Expression):
+        super().__init__((left_term, right_term))
+        self._left = left_term
+        self._right = right_term
+
+    def _calc(self):
+        return self._left.val + self._right.val
+
+
+class MultiplicationExpression(Expression):
+    def __init__(self, left_factor: Expression, right_factor: Expression):
+        super().__init__((left_factor, right_factor))
+        self._left = left_factor
+        self._right = right_factor
+
+    def _calc(self):
+        return self._left.val * self._right.val
 
 
 class Variable(Expression):
-    _by_name = {}
+    _by_name: dict[str, 'Variable'] = {}
+
+    @classmethod
+    def get_by_name(cls, name: str) -> 'Variable':
+        if name not in cls._by_name:
+            raise KeyError(f'No Variable called {name} exists')
+        return cls._by_name[name]
 
     def __init__(self, name: str):
-        super().__init__()
+        super().__init__(tuple())
         if name in self._by_name:
             raise ValueError(f'Variable with name {name} already exists')
         self._by_name[name] = self
+        self._dependent_exps: set[Expression] = set()
         self._name = name
+        self._assigned = False
+        self._val = None
+
+    def set(self, v):
+        if self._assigned:
+            raise VariableAssignmentError(f'Variable {self._name} is already assigned')
+        self._assigned = True
+        self._val = v
+
+    def unset(self):
+        if not self._assigned:
+            raise VariableAssignmentError(f'Variable {self._name} is not assigned')
+        self._assigned = False
+        super().unset()
+        for exp in self._dependent_exps:
+            exp.unset()
+
+    def _calc(self):
+        if not self._assigned:
+            raise VariableAssignmentError(f'Variable {self._name} is not assigned')
+        return self._val
+
+    def _add_dependent_expression(self, exp: Expression):
+        self._dependent_exps.add(exp)
+
+
+class AssignmentContext:
+    def __init__(self, assignments: dict[str, Any]):
+        self._assignments = assignments
+
+    def __enter__(self):
+        for name, val in self._assignments.items():
+            Variable.get_by_name(name).val = val
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for name in self._assignments:
+            Variable.get_by_name(name).unset()
 
 
 def d(num, denom):
@@ -57,8 +156,8 @@ def d(num, denom):
 
 
 def assign(**kwargs):
-    pass
+    return AssignmentContext(kwargs)
 
 
-def value(exp):
-    pass
+def value(exp: Expression):
+    return exp.val
--- autodiff/tests/scalar.py
+++ autodiff/tests/scalar.py
@@ -5,113 +5,139 @@ import autodiff
 class TestAutodiff(unittest.TestCase):
 
     def test_value(self):
-        x = autodiff.Variable('x')
-        with autodiff.assign(x=1):
-            self.assertEquals(autodiff.value(x), 1)
+        x = autodiff.Variable('x1')
+        with autodiff.assign(x1=1):
+            self.assertEqual(autodiff.value(x), 1)
 
     def test_const_addition_value(self):
-        x = autodiff.Variable('x')
+        x = autodiff.Variable('x2')
         s = x + 3
-        with autodiff.assign(x=1):
-            self.assertEquals(autodiff.value(s), 4)
+        with autodiff.assign(x2=1):
+            self.assertEqual(autodiff.value(s), 4)
 
     def test_const_addition_deriv(self):
-        x = autodiff.Variable('x')
+        x = autodiff.Variable('x3')
         s = x + 3
         dsdx = autodiff.d(s, x)
-        with autodiff.assign(x=1):
-            self.assertEquals(autodiff.value(dsdx), 1)
+        with autodiff.assign(x3=1):
+            self.assertEqual(autodiff.value(dsdx), 1)
 
     def test_addition_value(self):
-        x = autodiff.Variable('x')
-        y = autodiff.Variable('y')
+        x = autodiff.Variable('x4')
+        y = autodiff.Variable('y1')
         s = x + y
-        with autodiff.assign(x=1, y=2):
-            self.assertEquals(autodiff.value(s), 3)
+        with autodiff.assign(x4=1, y1=2):
+            self.assertEqual(autodiff.value(s), 3)
 
     def test_addition_deriv(self):
-        x = autodiff.Variable('x')
-        y = autodiff.Variable('y')
+        x = autodiff.Variable('x5')
+        y = autodiff.Variable('y2')
         s = x + y
         dsdx = autodiff.d(s, x)
         dsdy = autodiff.d(s, y)
-        with autodiff.assign(x=1, y=2):
-            self.assertEquals(autodiff.value(dsdx), 1)
-            self.assertEquals(autodiff.value(dsdy), 1)
+        with autodiff.assign(x5=1, y2=2):
+            self.assertEqual(autodiff.value(dsdx), 1)
+            self.assertEqual(autodiff.value(dsdy), 1)
 
     def test_self_deriv(self):
-        x = autodiff.Variable('x')
+        x = autodiff.Variable('x6')
         dxdx = autodiff.d(x, x)
-        with autodiff.assign(x=5):
-            self.assertEquals(autodiff.value(dxdx), 1)
+        with autodiff.assign(x6=5):
+            self.assertEqual(autodiff.value(dxdx), 1)
 
     def test_constant_mult_value(self):
-        x = autodiff.Variable('x')
+        x = autodiff.Variable('x7')
         y = 2 * x
-        with autodiff.assign(x=3):
-            self.assertEquals(autodiff.value(y), 6)
+        with autodiff.assign(x7=3):
+            self.assertEqual(autodiff.value(y), 6)
 
     def test_constant_mult_deriv(self):
-        x = autodiff.Variable('x')
+        x = autodiff.Variable('x8')
         y = 2 * x
         dydx = autodiff.d(y, x)
-        with autodiff.assign(x=3):
-            self.assertEquals(autodiff.value(dydx), 2)
+        with autodiff.assign(x8=3):
+            self.assertEqual(autodiff.value(dydx), 2)
 
     def test_mult_value(self):
-        x = autodiff.Variable('x')
-        y = autodiff.Variable('y')
+        x = autodiff.Variable('x9')
+        y = autodiff.Variable('y3')
         z = x * y
-        with autodiff.assign(x=3, y=4):
-            self.assertEquals(autodiff.value(z), 12)
+        with autodiff.assign(x9=3, y3=4):
+            self.assertEqual(autodiff.value(z), 12)
 
     def test_mult_deriv(self):
-        x = autodiff.Variable('x')
-        y = autodiff.Variable('y')
+        x = autodiff.Variable('x10')
+        y = autodiff.Variable('y4')
         z = x * y
         dzdx = autodiff.d(z, x)
         dzdy = autodiff.d(y, x)
-        with autodiff.assign(x=3, y=4):
-            self.assertEquals(autodiff.value(dzdx), 4)
-            self.assertEquals(autodiff.value(dzdy), 3)
+        with autodiff.assign(x10=3, y4=4):
+            self.assertEqual(autodiff.value(dzdx), 4)
+            self.assertEqual(autodiff.value(dzdy), 3)
 
     def test_power_value(self):
-        x = autodiff.Variable('x')
+        x = autodiff.Variable('x11')
         sq = x**2
-        with autodiff.assign(x=4):
-            self.assertEquals(autodiff.value(sq), 16)
+        with autodiff.assign(x11=4):
+            self.assertEqual(autodiff.value(sq), 16)
 
     def test_power_deriv(self):
-        x = autodiff.Variable('x')
+        x = autodiff.Variable('x12')
         sq = x**2
         dsqdx = autodiff.d(sq, x)
-        with autodiff.assign(x=4):
-            self.assertEquals(autodiff.value(dsqdx), 8)
+        with autodiff.assign(x12=4):
+            self.assertEqual(autodiff.value(dsqdx), 8)
 
     def test_linear_combo(self):
-        x = autodiff.Variable('x')
-        m = autodiff.Variable('m')
-        b = autodiff.Variable('b')
+        x = autodiff.Variable('x13')
+        m = autodiff.Variable('m1')
+        b = autodiff.Variable('b1')
         lin = m*x+b
         dlindx = autodiff.d(lin, x)
         dlindm = autodiff.d(lin, m)
         dlindb = autodiff.d(lin, b)
-        with autodiff.assign(x=5, m=3, b=2):
-            self.assertEquals(autodiff.value(lin), 17)
-            self.assertEquals(autodiff.value(dlindx), 3)
-            self.assertEquals(autodiff.value(dlindm), 5)
-            self.assertEquals(autodiff.value(dlindb), 1)
+        with autodiff.assign(x13=5, m1=3, b1=2):
+            self.assertEqual(autodiff.value(lin), 17)
+            self.assertEqual(autodiff.value(dlindx), 3)
+            self.assertEqual(autodiff.value(dlindm), 5)
+            self.assertEqual(autodiff.value(dlindb), 1)
 
     def test_dist_formula(self):
-        x = autodiff.Variable('x')
-        y = autodiff.Variable('y')
+        x = autodiff.Variable('x14')
+        y = autodiff.Variable('y5')
         z = (x**2+y**2)**0.5
         dzdx = autodiff.d(z, x)
         dzdy = autodiff.d(z, y)
-        with autodiff.assign(x=3, y=4):
-            self.assertEquals(autodiff.value(z), 5)
-            self.assertEquals(autodiff.value(dzdx), 3/5)
-            self.assertEquals(autodiff.value(dzdy), 4/5)
+        with autodiff.assign(x14=3, y5=4):
+            self.assertEqual(autodiff.value(z), 5)
+            self.assertEqual(autodiff.value(dzdx), 3/5)
+            self.assertEqual(autodiff.value(dzdy), 4/5)
+
+    def test_mult_assignments(self):
+        x = autodiff.Variable('x15')
+        y = autodiff.Variable('y6')
+        z = x + y
+        with autodiff.assign(x15=3, y6=4):
+            self.assertEqual(autodiff.value(z), 7)
+        with autodiff.assign(x15=5, y6=3):
+            self.assertEqual(autodiff.value(z), 8)
+
+    def test_unassigned_error(self):
+        x = autodiff.Variable('x16')
+        y = autodiff.Variable('y7')
+        z = x + y
+        with self.assertRaises(autodiff.VariableAssignmentError):
+            autodiff.value(z)
+
+    def test_unassigned_after_assigned_error(self):
+        x = autodiff.Variable('x17')
+        y = autodiff.Variable('y8')
+        z = x + y
+        with autodiff.assign(x17=3, y8=4):
+            self.assertEqual(autodiff.value(z), 7)
+
+        with self.assertRaises(autodiff.VariableAssignmentError):
+            autodiff.value(z)
 
 
 if __name__ == '__main__':
